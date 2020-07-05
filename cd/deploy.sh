702b90e04fad7383f4c258f88bfd6b56b92e5b05
Sebastian Gellweiler <sg@gellweiler.net>
2020-07-05 16:01:30 +0200

    Fix travis-ci build.

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
-openssl aes-256-cbc -K $encrypted_e05aca184678_key -iv $encrypted_e05aca184678_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
+openssl aes-256-cbc -K $encrypted_81c98acad902_key -iv $encrypted_81c98acad902_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
 gpg --fast-import cd/codesigning.asc
 mvn deploy -P sign,build-extras --settings cd/mvnsettings.xml
